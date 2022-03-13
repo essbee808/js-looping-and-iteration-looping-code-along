76dada5ecb2a268ab3e20939d75df6c1d689211f
Sheena Bonilla <sheena.myb@gmail.com>
2022-03-12 21:58:41 -0800

    commit tree d4f23e9e1ff155abc18a4828b5eee5089f7da8b1

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solutions in this file
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//     for (let i = 0; i < gifts.length; i++) {
+//         console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//     }
+//     return gifts;
+// }
+
+function writeCards(namesArray, event) {
+    let cards = []
+
+    for (let i = 0; i < namesArray.length; i++) {
+        cards.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`)
+    }
+    return cards;
+}
+
+writeCards(["Lisa", "Kaitlin", "Jan"], "surprise");
+
+function wrapGifts(gifts) {
+    let i = 0; // the initialization moves outside the body of the loop!
+    while (i < gifts.length) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        i++; //iteration moves inside the body of the loop
+    }
+    return gifts;
+}
+
+wrapGifts(gifts);
+
+function countDown() {
+    let i = 10;
+    while (i >= 0) {
+        console.log(i--);
+    }
+}
+
